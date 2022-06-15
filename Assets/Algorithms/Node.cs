85308b6f9e3df45b8e1601f9dbad83673eceadf7
João Azuaga <joao.azuaga@gmail.com>
2022-06-15 15:59:31 +0200

    Added patroller with pseudo-linked list

---
+++ Assets/Algorithms/Node.cs
@@ -0,0 +1,16 @@
+﻿//Author: João Azuaga
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UnityIntro.Algorithms {
+	public class Node : MonoBehaviour {
+
+		public Node Next;
+
+	}
+}
---
+++ Assets/Algorithms/Patroller.cs
@@ -0,0 +1,40 @@
+﻿//Author: João Azuaga
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UnityIntro.Algorithms {
+
+	public class Patroller : MonoBehaviour {
+
+		public Transform[] Waypoints;
+
+		private int _current;
+
+		public Node StartNode;
+		public Node CurrentNode;
+
+
+		// Use this for initialization
+		private void Start() {
+			CurrentNode = StartNode;
+		}
+
+		// Update is called once per frame
+		private void Update() {
+			//if(Vector3.Distance(Waypoints[_current].position, transform.position) < 0.01f)
+			//	_current++;
+
+			//transform.Translate((Waypoints[_current].position - transform.position).normalized * Time.deltaTime);
+
+			if(Vector3.Distance(CurrentNode.transform.position, transform.position) < 0.01f)
+				CurrentNode = CurrentNode.Next;
+
+			transform.Translate((CurrentNode.transform.position - transform.position).normalized * Time.deltaTime);
+		}
+	}
+}
