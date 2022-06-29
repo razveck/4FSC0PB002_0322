acdd146806e55f85d6a61931d8b57aa2c844b3ac
João Azuaga <joao.azuaga@gmail.com>
2022-06-29 16:24:46 +0200

    Stacks, queues and trees

---
+++ Assets/Algorithms/GenericTree.cs
@@ -0,0 +1,58 @@
+﻿//Author: João Azuaga
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UnityIntro.Erik.TowerDefense.Assets.Algorithms {
+	public class GenericTree<T> {
+		private T[] array;
+
+		public Func<T, T, bool> SortingCondition;
+
+		public void Sort() {
+			for(int i = 0; i < array.Length - 1; i++) {
+				bool isSorted = SortingCondition(array[i], array[i + 1]);
+				//if(!isSorted)
+				//Swap(array[i], array[i + 1]);
+			}
+		}
+	}
+
+	public class Program {
+		void Main() {
+			var tree = new GenericTree<int>();
+			tree.SortingCondition = (a, b) => {
+				//if a < b, return true
+				//else return false
+				return a > b;
+			};
+
+			var GOtree = new GenericTree<GameObject>();
+			GOtree.SortingCondition = (a, b) => {
+				return a.transform.childCount < b.transform.childCount;
+			};
+
+			var transformList = new List<Transform>();
+			transformList.Sort((a, b) => {
+				if(a.childCount < b.childCount)
+					return -1;
+				else
+					return 1;
+
+			});
+		}
+	}
+
+	public class MyComparer : IComparer<int> {
+		public int Compare(int a, int b) {
+			if(a < b)
+				return -1;
+			else
+				return 1;
+		}
+	}
+}
---
+++ Assets/Algorithms/StackQueue.cs
@@ -0,0 +1,33 @@
+﻿//Author: João Azuaga
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UnityIntro.Erik.TowerDefense.Assets.Algorithms {
+	public class StackQueue : MonoBehaviour {
+
+		public Stack<int> MyStack = new();
+		public Queue<int> MyQueue = new();
+
+		// Use this for initialization
+		private void Start() {
+			MyStack.Push(1);
+			MyStack.Push(2);
+			print(MyStack.Pop());
+
+			MyQueue.Enqueue(1);
+			MyQueue.Enqueue(2);
+			print(MyQueue.Dequeue());
+
+		}
+
+		// Update is called once per frame
+		private void Update() {
+
+		}
+	}
+}
