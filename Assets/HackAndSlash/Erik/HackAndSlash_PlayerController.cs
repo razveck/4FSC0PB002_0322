62fe7cefa6672e0404d1e07a86bdcd6cea1d1415
João Azuaga <joao.azuaga@gmail.com>
2022-06-30 12:13:45 +0200

    Erik rotation code

--- Assets/HackAndSlash/Erik/HackAndSlash_PlayerController.cs
+++ Assets/HackAndSlash/Erik/HackAndSlash_PlayerController.cs
@@ -30,6 +30,7 @@ namespace UnityIntro.Erik.HackAndSlash
             input.Player.Dash.performed += ctx => dashFlag = true;
 
             input.Player.MousePos.performed += ctx => mousePos = ctx.ReadValue<Vector2>();
+            navAgent.updateRotation = false;
         }
 
         private void Update() {
@@ -51,13 +52,22 @@ namespace UnityIntro.Erik.HackAndSlash
             dashFlag = false;
         }
         void Rotate(Vector2 mousePos){
-            Vector3 position = new Vector3(mousePos.x, mousePos.y, 0);
-            Ray hitRay = Camera.main.ScreenPointToRay(position, Camera.MonoOrStereoscopicEye.Mono);
+            //Vector3 position = new Vector3(mousePos.x, mousePos.y, 0);
+            //Ray hitRay = Camera.main.ScreenPointToRay(position, Camera.MonoOrStereoscopicEye.Mono);
             
-            Physics.Raycast(hitRay, out RaycastHit rh);
-            Vector3 dir = (rh.point - transform.position).normalized;
+            //Physics.Raycast(hitRay, out RaycastHit rh);
+            //Vector3 dir = (rh.point - transform.position).normalized;
 
-            transform.forward = transform.position + dir;
+            //transform.forward = transform.position + dir;
+
+            
+            var screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+            Vector3 dir = ((Vector3)mousePos - screenPoint).normalized;
+
+            var worldDir = new Vector3(dir.x, 0, dir.y);
+
+            transform.forward = worldDir;
+            
         }
 
         private void OnEnable() => input.Enable();
