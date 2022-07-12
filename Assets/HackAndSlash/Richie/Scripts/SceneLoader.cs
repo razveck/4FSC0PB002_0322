9f5e28cd8c60a77bd37b105239cbb856d73af154
João Azuaga <joao.azuaga@gmail.com>
2022-07-12 12:41:44 +0200

    Added screen flow to hack and slash

---
+++ Assets/HackAndSlash/Richie/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿//Author: João Azuaga
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityIntro.HackAndSlash.Richie.Scripts {
+	public class SceneLoader : MonoBehaviour {
+
+		public void Load(int buildIndex){
+			SceneManager.LoadScene(buildIndex);
+		}
+
+	}
+}
---
+++ Assets/HackAndSlash/Richie/Scripts/SetLoadCommand.cs
@@ -0,0 +1,18 @@
+﻿//Author: João Azuaga
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Richie;
+using UnityEngine;
+
+namespace UnityIntro.HackAndSlash.Richie.Scripts {
+	public class SetLoadCommand : MonoBehaviour {
+
+		public void SetLoad(bool value){
+			SaveSystem.loadFromSave = value;
+		}
+	}
+}
